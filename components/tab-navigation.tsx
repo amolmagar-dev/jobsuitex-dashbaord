914e2a48db45427f28d4a6bbbfa3695f1dafc229
Amol Magar <amolmagar.dev@gmail.com>
2025-04-20 15:47:43 +0530

    fix: update site header title to 'JobSuiteX' and replace sidebar icon with BriefcaseBusiness

--- components/tab-navigation.tsx
+++ components/tab-navigation.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { usePathname, useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
 
 export function TabNavigation() {
   const router = useRouter()
